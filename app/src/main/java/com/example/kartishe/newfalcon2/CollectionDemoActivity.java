7b8b960e467fc4d85413d057f0464a54507f457c
System Administrator <kartishe@cisco.com>
2016-01-21 19:29:41 -0800

    first Repo commit

---
+++ app/src/main/java/com/example/kartishe/newfalcon2/CollectionDemoActivity.java
@@ -0,0 +1,63 @@
+package com.example.kartishe.newfalcon2;
+
+/**
+ * Created by kartishe on 10/14/15.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+
+public class CollectionDemoActivity extends FragmentActivity {
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+    ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mDemoCollectionPagerAdapter =
+                new DemoCollectionPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+}
+
+// Since this is an object collection, use a FragmentStatePagerAdapter,
+// and NOT a FragmentPagerAdapter.
+class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
+    public DemoCollectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = new FalconCSRInformation();
+        Bundle args = new Bundle();
+        // Our object is just an integer :-P
+        args.putInt(FalconCSRInformation.ARG_OBJECT, i + 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 100;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "OBJECT " + (position + 1);
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/kartishe/newfalcon2/FalconDeviceListActivity.java
@@ -0,0 +1,47 @@
+package com.example.kartishe.newfalcon2;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by amitkumarsingh on 1/21/16.
+ */
+public class FalconDeviceListActivity extends android.app.ListActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_falcon2);
+        // storing string resources into Array
+        String[] adobe_products = {"a","b","c"};
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label1, adobe_products));
+        ListView lv = getListView();
+
+        // listening to single list item on click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String product = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), CollectionDemoActivity.class);
+                // sending data to new activity
+                i.putExtra("product", product);
+                startActivity(i);
+
+
+            }
+        });
+
+    }
+}
